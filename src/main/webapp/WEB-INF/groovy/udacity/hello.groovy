8de2d5b3e6a3b4bb16cc7dc9b039729c54b7adb2
Denis Windsor <windsor.denis@googlemail.com>
2012-04-29 11:40:56 +0100

    script for the first udacity hm refactoring

--- src/main/webapp/WEB-INF/groovy/udacity/hello.groovy
+++ src/main/webapp/WEB-INF/groovy/udacity/hello.groovy
@@ -2,4 +2,4 @@ request.setAttribute 'contentHtml', 'hello_udacity.html'
 request.setAttribute 'contentTitle', 'Hello, udacity!'
 request.setAttribute 'contentBodyId', 'www-deniswindsor-co-uk-hello'
 
-forward '/WEB-INF/templates/hello.html'
+forward '/WEB-INF/templates/udacity/hello.html'
